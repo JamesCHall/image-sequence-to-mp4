5f21d1303fd27349fc436e97121cacc02874a778
Jono Hall <jonohall@gmail.com>
2021-07-29 21:08:09 +1200

    Refactored to single file

--- convert_exrSeq-mp4.bat
+++ convert_exrSeq-mp4.bat
@@ -1,17 +1,32 @@
 @echo off
 
-del /q "C:\ffmpeg\bin\images.txt"
-
-set /p path=Image sequence path:
-set /p framerate=Enter framerate:
 cls
 
-for %%a in ("%path%\*.exr") do (
-    echo file 'file:%%a' >> "C:\ffmpeg\bin\images.txt"
+set exr_param=
+
+set /p filepath=Image sequence path:
+set /p framerate=Enter framerate (25): || set framerate=25
+choice /N /C:pje /M "Is this sequence (P)NG, (J)PG or (E)XR:"%1
+
+IF ERRORLEVEL ==1 (
+    set filetype=png    
+)
+IF ERRORLEVEL ==2 (
+    set filetype=jpg
+)
+IF ERRORLEVEL ==3 (
+    set filetype=exr
+    set exr_param="-apply_trc iec61966_2_1 "
+)
+cls
+
+if exist "%filepath%\images.txt" (
+    del /q "%filepath%\images.txt"
+)
+for %%a in ("%filepath%\*.%filetype%") do (
+    echo file 'file:%%a' >> "%filepath%\images.txt"
 )
 
-C:\ffmpeg\bin\ffmpeg.exe -apply_trc iec61966_2_1 -r %framerate% -f concat -safe 0 -i "C:\ffmpeg\bin\images.txt" -c:v libx264 -pix_fmt yuv420p "%path%\output.mp4"
+ffmpeg\bin\ffmpeg.exe %exr_param% -r %framerate% -f concat -safe 0 -i "%filepath%\images.txt" -c:v libx264 -pix_fmt yuv420p "%filepath%\output.mp4"
 
-pause
-
-del /q "C:\ffmpeg\bin\images.txt"
+del /q "%filepath%\images.txt"
